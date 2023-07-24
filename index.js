9d9ae354639d338b96977ccbbd09ecdd0c1e2f2e
Naol <37399395+Belayne@users.noreply.github.com>
2023-07-24 17:24:02 +0200

    Add functions to create new books

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+const addBookBtn = document.querySelector('.add-btn');
+const bookFormDiv = document.querySelector('.form-div');
+const form = document.querySelector('form');
 
+const addBookFormBtn = document.querySelector('#add-book');
+const cancelFormBtn = document.querySelector('#cancel-form');
+
+const formBookTitle = document.querySelector('#title');
+const formBookAuthor = document.querySelector('#author');
+const formBookPages = document.querySelector('#pages');
+const formBookSummary = document.querySelector('#summary');
+const formBookRead = document.querySelector('#read-yes');
 
 let myLibrary = [];
 
@@ -18,3 +29,31 @@ function Book(title, author, pages, summary, read) {
     }
 }
 
+addBookBtn.addEventListener('click', showForm)
+cancelFormBtn.addEventListener('click', hideForm)
+addBookFormBtn.addEventListener('click', createBook);
+
+function showForm() {
+    bookFormDiv.classList.remove('hidden')
+};
+
+function hideForm() {
+    bookFormDiv.classList.add('hidden')
+};
+
+function createBook() {
+    let title = formBookTitle.value;
+    let author = formBookAuthor.value;
+    let pages = formBookPages.value;
+    let summary = formBookSummary.value;
+    let read = formBookRead.checked
+    const newBook = new Book(title, author, pages, summary, read)
+    addBook(newBook)
+}
+
+function addBook(newBook) {
+    console.log(newBook);
+    myLibrary.push(newBook);
+    hideForm();
+    form.reset();
+}
